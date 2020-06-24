758e6908ca1c72aa494fa0aa1534673c72806435
Z415 <62089292Z415@users.noreply.github.com>
2020-06-24 12:57:01 +0800

    index.html

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("hi");
+function renderEditor(){
+ let inputE1 =  document.querySelector("#default-todo-panel .todo-editor > input");
+ inputE1.onchange = (e) => {
+     console.log("text,",e.target.value);
+     //console.log("input change:",e);
+ };
+}
+
+renderEditor();
