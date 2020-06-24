5bfbd6a8e3b14ff64e2f1b784fde28e6fb2c8514
Z415 <62089292Z415@users.noreply.github.com>
2020-06-24 15:24:38 +0800

    这是我的作业

--- script.js
+++ script.js
@@ -1,9 +1,75 @@
+
+let tasks = [];//{title:"dddddd",done :false}
+
 function renderEditor(){
- let inputE1 =  document.querySelector("#default-todo-panel .todo-editor > input");
- inputE1.onchange = (e) => {
-     console.log("text,",e.target.value);
-     //console.log("input change:",e);
- };
+ let inputEl =  document.querySelector("#default-todo-panel .todo-editor > input");
+ //inputEl.onchange = (e) => {
+ //    console.log("text,",e.target.value);
+ //    //console.log("input change:",e);
+// };
+
+let addTask = ()=>{
+    let newTask ={
+        title: inputEl.value,
+        done: false,
+    };
+    inputEl.value ="";
+  
+    tasks.push(newTask);
+    
+    console.log("tasks: ",tasks);
+  
+    renderTaskItems();
+
 }
 
+
+inputEl.onkeypress = (e) => {
+
+  if (e.key==="Enter"){
+    addTask();
+  }
+};
+
+let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
+addEl.onclick =(e) => {
+  addTask();
+  
+};
+
+}
+function renderTaskItems(){
+    console.log("render items");
+  let itemsEl =  document.querySelector("#default-todo-panel .todo-items");
+
+  itemsEl.querySelectorAll("div").forEach((Node)=>Node.remove());
+
+
+  for (let  i= 0; i< tasks.length; i++ ){
+      let task = tasks[i];
+      let itemEl = document.createElement("div");
+
+
+      let doneEl = document.createElement("input");
+      doneEl.type = "checkbox";
+      itemEl.append(doneEl);
+
+      let titleEl = document.createElement("label");
+      titleEl.innerText = task.title;
+      itemEl.append(titleEl);
+
+      let cancelEl = document.createElement("button");
+      cancelEl.innerText = "x";
+      cancelEl.onclick = () =>{
+          tasks.splice(i, 1);
+          renderTaskItems();
+      };
+      itemEl.append(cancelEl);
+
+      itemsEl.append(itemEl);
+
+
+  }
+}
 renderEditor();
+renderTaskItems();
